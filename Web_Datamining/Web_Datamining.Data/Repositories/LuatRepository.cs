cee08c19352681493be10d55d87bb9b6778ba7ef
Tran Dinh Son <trandinhson2112cntt@gmail.com>
2018-10-22 22:17:35 +0700

    Đây là lần commit sửa cấu trúc cuốicùng

---
+++ Web_Datamining/Web_Datamining.Data/Repositories/LuatRepository.cs
@@ -0,0 +1,16 @@
+﻿using Web_Datamining.Data.Infrastructure;
+using Web_Datamining.Models;
+
+namespace Web_Datamining.Data.Repositories
+{
+    public interface ILuatRepository : IRepository<Luat>
+    {
+    }
+
+    public class LuatRepository : RepositoryBase<Luat>, ILuatRepository
+    {
+        public LuatRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
